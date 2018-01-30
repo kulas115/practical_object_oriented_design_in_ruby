d6891a47dc9ee6ea656c074b3219f839d350b9d4
Kulas115 <kulas115@gmail.com>
2018-01-30 21:44:03 +0100

    Moved wheel to separate class due to new method requirement - circumference

--- bicycle.rb
+++ bicycle.rb
@@ -1,10 +1,9 @@
 class Gear
-  attr_reader :chainring, :cog, :rim, :tire
-  def initialize(chainring, cog, rim, tire)
+  attr_reader :chainring, :cog, :wheel
+  def initialize(chainring, cog, wheel=nil)
     @chainring = chainring
     @cog       = cog
-    @rim       = rim
-    @tire      = tire
+    @wheel     = wheel
   end
 
   def ratio
@@ -12,17 +11,30 @@ class Gear
   end
 
   def gear_inches
-    # tire goes around rim twice for diameter
     ratio * wheel.diameter
-  end
-
-  Wheel = Struct.new(:rim, :tire) do
-    def diameter
-      rim + (tire * 2)
-    end
   end
 end
 
-puts Gear.new(52,11,26,2.25).gear_inches
+class Wheel
+  attr_reader :rim, :tire
 
-puts Gear.new(30,27,28, 1.25).gear_inches
+  def initialize(rim, tire)
+    @rim       = rim
+    @tire      = tire
+  end
+
+    def diameter
+      rim + (tire * 2)
+    end
+
+    def circumference
+      diameter * Math::PI
+  end
+end
+
+@wheel = Wheel.new(26, 1.5)
+puts @wheel.circumference
+
+puts Gear.new(52, 11, @wheel).gear_inches
+
+puts Gear.new(52,11).ratio
