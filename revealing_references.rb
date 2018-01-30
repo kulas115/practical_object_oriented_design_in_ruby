b3edd9a3bc7f95fa503f489f915b5bfa9c9d494e
Kulas115 <kulas115@gmail.com>
2018-01-30 21:26:52 +0100

    New exercise for single responsibility in revealing_references file

---
+++ revealing_references.rb
@@ -0,0 +1,17 @@
+class RevealingReferences
+  attr_reader :wheels
+  def initialize(data)
+    @wheels = wheelify(data)
+  end
+
+  def diameters
+    wheels.collect { |wheel|
+      wheel.rim + (wheel.rim * 2) }
+  end
+
+  Wheel = Struct.new(:rim, :tire)
+  def wheelify(data)
+    deta.collect {|cell|
+      Wheel.new(cell[0]m cell[1]) }
+  end
+end
