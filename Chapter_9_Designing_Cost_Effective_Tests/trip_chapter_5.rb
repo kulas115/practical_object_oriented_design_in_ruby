57969693f80819293c356d5d902ba89a99b7a588
Kulas115 <kulas115@gmail.com>
2018-02-09 17:31:30 +0100

    Testing duck types

---
+++ Chapter_9_Designing_Cost_Effective_Tests/trip_chapter_5.rb
@@ -0,0 +1,60 @@
+class Trip
+  attr_reader :bicycles, :customers, :vehicle
+
+  def prepare(preparers)
+    preparers.each { |preparer|
+      preparer.prepare_trip(self) }
+  end
+end
+
+class Mechanic
+  def prepare_trip(trip)
+    trip.bicycles.each {|bicycle|
+      prepare_bicycle(bicycle)}
+  end
+
+class TripCoordinator
+  def prepare_trip(trip)
+    buy_food(trip.customers)
+  end
+end
+
+class Driver
+  def prepare_trip(trip)
+    vehicle = trip.vehicle
+    gas_up(vehicle)
+    fill_water_tank(vehicle)
+  end
+end
+
+module PreparerInterfaceTest
+  def test_implements_the_preparer_interface
+    assert_respond_to(@object, :prepare_trip)
+  end
+end
+
+class MechanicTest < MiniTest::Unit::TestCase
+  include PreparerInterfaceTest
+
+  def setup
+    @mechanic = @object = Mechanic.new
+  end
+
+  # placeholder for other tests that rely on mechanic
+end
+
+class TripCoordinatorTest < MiniTest::Unit::TestCase
+  include PreparerInterfaceTest
+
+  def setup
+    @trip_coordinator = @object = TripCoordinator.new
+  end
+end
+
+class DriverTest < MiniTest::Unit::TestCase
+  include PreparerInterfaceTest
+
+  def setup
+    @driver = @object = Driver.new
+  end
+end
