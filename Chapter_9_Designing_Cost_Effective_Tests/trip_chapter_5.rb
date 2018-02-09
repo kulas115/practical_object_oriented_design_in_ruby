72bbe447d1d9a11bb807e9f93ffe899df6b108f1
Kulas115 <kulas115@gmail.com>
2018-02-09 17:32:12 +0100

    Testing duck types

--- Chapter_9_Designing_Cost_Effective_Tests/trip_chapter_5.rb
+++ Chapter_9_Designing_Cost_Effective_Tests/trip_chapter_5.rb
@@ -58,3 +58,15 @@ class DriverTest < MiniTest::Unit::TestCase
     @driver = @object = Driver.new
   end
 end
+
+class TripTest < MiniTest::Unit::TestCase
+
+  def test_requests_trip_preparation
+    @preparer = MiniTest::Mock.new
+    @trip     = Trip.new
+    @preparer.expect(:prepare_trip, nil, [@trip])
+
+    @trip.prepare([@preparer])
+    @preparer.verify
+  end
+end
